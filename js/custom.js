4dd569c5a7182b1f7e413174c1f7c1616ec536fa
hadithmv <30223914+hadithmv@users.noreply.github.com>
2020-05-10 15:24:24 +0300

    added links

--- js/custom.js
+++ js/custom.js
@@ -435,7 +435,7 @@ $(document).ready(() => {
 
       {
         extend: 'colvis',
-        key: { key: 'h', shiftKey: true },
+        key: { key: 's', shiftKey: true },
         text: 'އިތުރު ބަރިތައް',
         background: false /* removes background fade animation for collection */
       } // end of colvis
@@ -533,3 +533,8 @@ $(document).ready(() => {
   // adds a placeholder to above <input class="paginate_input" type="text">
   $('.paginate_input').attr('placeholder', 'ސަފުހާ...')
 }) // ==================== END OF $(document).ready( function () {
+
+/* Desktop only keyboard nav help Alert */
+function myHelp () {
+  alert('Keyboard Controls: \n\n [Tab] =Tab Navigation \n [Arrow Keys] = Keyboard Navigation \n [Shift + s] = Show columns \n [Shift + c] = Copy')
+} /*  \n [Shift + x] = Excel \n [Shift + v] = Csv \n [Shift + p] = Print' */
