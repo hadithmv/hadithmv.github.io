af3a719074490eb979aa422245c60378034f9422
yahyasdadmv@gmail.com <yahyasdadmv@gmail.com>
2020-10-07 03:35:53 +0500

    u
    Former-commit-id: 68a04972789541ab5b0c0a693f659e0ff59936f9

--- js/umdah-script.js
+++ js/umdah-script.js
@@ -521,8 +521,3 @@ $(document).ready(() => {
   // adds a placeholder to above <input class="paginate_input" type="text">
   $('.paginate_input').attr('placeholder', 'ސަފުހާ...')
 }) // ==================== END OF $(document).ready( function () {
-
-/* Desktop only keyboard nav help Alert */
-function myHelp () {
-  alert('Keyboard Controls: \n\n [Tab] =Tab Navigation \n [Arrow Keys] = Keyboard Navigation \n [Shift + s] = Show columns \n [Shift + c] = Copy')
-} /* \n [Shift + x] = Excel \n [Shift + v] = Csv \n [Shift + p] = Print' */
