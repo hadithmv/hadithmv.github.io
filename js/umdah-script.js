71457869715bab91dc58cd535e736273882c3446
yahyasdadmv@gmail.com <yahyasdadmv@gmail.com>
2020-10-10 05:54:08 +0500

    u
    Former-commit-id: 29a3ed5d707ce68c8db131d690da52db5dae1107

--- js/umdah-script.js
+++ js/umdah-script.js
@@ -325,7 +325,7 @@ $(document).ready(() => {
       infoEmpty: '- ނުފެނުނު -',
       lengthMenu: '_MENU_',
       search: '', // Originally "Search:" leave this blank in production
-      searchPlaceholder: 'ޢުމްދާއިން ހޯއްދަވާ...',
+      searchPlaceholder: 'ޢުމްދަތުލް އަޙްކާމިން ހޯއްދަވާ...',
       zeroRecords: '- ނުފެނުނު -'
     }, //= =================== End of Internationalisation
 
