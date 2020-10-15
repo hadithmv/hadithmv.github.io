5fbfdd5cd3b219a574ed8252b6f7573bd34fa05a
yahyasdadmv@gmail.com <yahyasdadmv@gmail.com>
2020-10-15 22:23:42 +0500

    u
    Former-commit-id: 150db4ce2d017ea6dbb5aab3fa2ded5468ac5399
    Former-commit-id: 0de6aa147122f6fbeaeb54392fd8725aadc06e49

--- js/sahihain-script.js
+++ js/sahihain-script.js
@@ -232,7 +232,8 @@ $(document).ready(() => {
     // or 2Darray where 1st inner array=page length values, 2nd displayed options
     // -1 is used as a value this tells DataTables to disable pagination
     // Default [ 10, 25, 50, 100 ],
-    lengthMenu: [[1, 2, 3, 5, 7, 10, 15, 20, -1], ['1 ދައްކާ', 2, 3, 5, 7, 10, 15, 20, 'ހުރިހައި']],
+    lengthMenu: [[1, 2, 3, 5, 10, 20, 30, 50], ['1 ދައްކާ', 2, 3, 5, 10, 20, 30, '50']],
+    // lengthMenu: [[1, 2, 3, 5, 7, 10, 15, 20, -1], ['1 ދައްކާ', 2, 3, 5, 7, 10, 15, 20, 'ހުރިހައި']],
     // lengthMenu: [ [5, 10, 20, 30, 40, -1, 1], ["Show 5", 10, 20, 30, 40,
     // "All", 1] ],
 
