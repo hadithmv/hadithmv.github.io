0dfb687238dc9fc17a9c6fa83bf19193dec9df52
yahyasdadmv@gmail.com <yahyasdadmv@gmail.com>
2020-10-25 19:35:21 +0500

    u
    Former-commit-id: 2af1af74271a313f5483159bd787bb8bc0311b15
    Former-commit-id: 28676a82401dc62dbe627a73e8ec2dcf484c8688

--- js/sahihain-script.js
+++ js/sahihain-script.js
@@ -1,26 +1,6 @@
 // global document, window, alert, console, require
 
 // =====================
-//       TABS
-// ====================
-function openLink (evt, tabName) {
-  // added to get rid of error Missing "use strict" statement.
-  let i
-  x
-  tablinks
-  x = document.getElementsByClassName('myTab')
-
-  for (i = 0; i < x.length; i += 1) {
-    // changed due to Expected "+= 1" and instead saw "++".
-    x[i].style.display = 'none'
-  }
-  tablinks = document.getElementsByClassName('tablink')
-
-  document.getElementById(tabName).style.display = 'block'
-}
-// ===== ===== Tabs - End ===== =====
-
-// =====================
 //      DT JS BELOW
 // ====================
 $(document).ready(() => {
