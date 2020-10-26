b013d92803ef53255d6c687eef525c37b5e9ad04
yahyasdadmv@gmail.com <yahyasdadmv@gmail.com>
2020-10-26 20:47:25 +0500

    u
    Former-commit-id: 5ef9ca52a967a89766583e27c2afc37fa9b96303
    Former-commit-id: 077b3b35d3df5a5f10ff852ac04be2f990fdb1cc

--- js/radheef-script.js
+++ js/radheef-script.js
@@ -325,22 +325,17 @@ $(document).ready(() => {
           // data = data.replace(/r\n]/g, '') // needed to make rnr work
           // data = data.replace(/\r\n|\n|\t/gm, '')
 
-          data = data.replace(/\r\n\r\n/g, ' ') //  ONLY FOR 40N, because it has an inserted title
-          data = data.replace(/\n\n/g, ' ') //  ONLY FOR 40N, because it has an inserted title, the two lines below arent needed for 40n i think
+          // data = data.replace(/\r\n\r\n/g, ' ') //  ONLY FOR 40N, because it has an inserted title
+          // data = data.replace(/\n\n/g, ' ') //  ONLY FOR 40N, because it has an inserted title, the two lines below arent needed for 40n i think
 
           data = data.replace(/\r\n/g, '\t') //  prevents first header showing up unneeded (windows)
           data = data.replace(/\n/g, '\t') //  prevents first header showing up unneeded (linux) this needs to go below windows rn
 
-          data = data.replace(/#\t/g, '') // should be this way instead of /\tފޮތް/
-          data = data.replace(/ތަޚްރީޖު\t/g, '')
-          data = data.replace(/ޢަރަބި ސުރުޚީ\t/g, '')
-          data = data.replace(/ދިވެހި ސުރުޚީ\t/g, '')
-          data = data.replace(/ޢަރަބި ޙަދީޘް\t/g, '')
+          data = data.replace(/ޢަރަބި ލަފްޒު\t/g, '') // should be this way instead of /\tފޮތް/
           data = data.replace(/ޢަރަބި ފިލިނުޖަހައި\t/g, '')
-          data = data.replace(/ދިވެހި ތަރުޖަމާ\t/g, '')
-          data = data.replace(/ތަޚްރީޖު\t/g, '')
-          data = data.replace(/ތަޚްރީޖު ދިވެހިން\t/g, '')
-          data = data.replace(/ރިޔާޟުއްޞާލިޙީނުން\t/g, '')
+          data = data.replace(/ޢަރަބި ބަޔާން\t/g, '')
+          data = data.replace(/ދިވެހި ލަފްޒު\t/g, '')
+          data = data.replace(/އިނގިރޭސި ލަފްޒު\t/g, '')
 
           data = data.replace(/\t\t/g, '\t') //  This prevents a double or more line breaks when columns are hidden
           data = data.replace(/\t/g, '\n\n') // creates line breaks between cell data
