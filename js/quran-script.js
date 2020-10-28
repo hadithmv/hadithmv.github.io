948d5c86ed27aa1d8e3317b4afdc4e63123d1396
yahyasdadmv@gmail.com <yahyasdadmv@gmail.com>
2020-10-28 12:25:04 +0500

    u
    Former-commit-id: 3d36837b932baa2c65084d9a0e6f5097685f653a
    Former-commit-id: b893f6d6be9b80d5a8f3154699a2db22e171d842

--- js/quran-script.js
+++ js/quran-script.js
@@ -311,7 +311,7 @@ $(document).ready(() => {
         extend: 'copy',
         key: { key: 'c', shiftKey: true },
         text: 'ކޮޕީ',
-        messageTop: 'ޙަދީޘްއެމްވީ - ކީރިތި ޤުރްއާން', // CHANGE123 clipboard message
+        messageTop: 'ޙަދީޘްއެމްވީ - ޤުރްއާނުގެ ތަރުޖަމާއާއި ތަފްސީރު', // CHANGE123 clipboard message
         title: '' /* title: "hadithmv.com", */,
 
         //= ====================
