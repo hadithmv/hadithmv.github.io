221245ab5b46f23c04c955eb3b13803434a7554a
yahyasdadmv@gmail.com <yahyasdadmv@gmail.com>
2020-11-01 20:14:53 +0500

    u
    Former-commit-id: 826f2e6fb97f960f10a4432b5b7519262dd342bd
    Former-commit-id: 2f36623253c5eb80741ac1a2d5150c2db9b1f027

--- js/barbahari-script.js
+++ js/barbahari-script.js
@@ -79,13 +79,13 @@ $(document).ready(() => {
       { /* instead of repeating this part of the array within the external json,
          we can strip diacritics using regex within the table itself, this makes
          the array file much smaller in the long run */
-        data: 1,
+        /* data: 1,
         title: 'ޢަރަބި ފިލިނުޖަހައި',
-        render: function (data, type, row) {
-          // return data.replace(/َ/g, '').replace(/ِ/g, '')
-          // below code is shorter, no replace repeat, uses OR instead
-          return data.replace(/ّ|َ|ً|ُ|ٌ|ِ|ٍ|ْ/g, '')
-        }
+        render: function (data, type, row) { */
+        // return data.replace(/َ/g, '').replace(/ِ/g, '')
+        // below code is shorter, no replace repeat, uses OR instead
+        /* return data.replace(/ّ|َ|ً|ُ|ٌ|ِ|ٍ|ْ/g, '')
+        } */
       },
       {
         data: 2,
