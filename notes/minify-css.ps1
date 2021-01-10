e422d10cf0eb8899c6aa076c2db3f0f892bfa0a6
yahyasdadmv@gmail.com <yahyasdadmv@gmail.com>
2021-01-11 04:34:43 +0500

    u

---
+++ notes/minify-css.ps1
@@ -0,0 +1,5 @@
+csso --input C:\Users\ashraaf\Downloads\VScode\hadithmv.github.io\notes\minimal-mod.css --output C:\Users\ashraaf\Downloads\VScode\hadithmv.github.io\notes\minimal-mod.min.css
+
+Copy-Item C:\Users\ashraaf\Downloads\VScode\hadithmv.github.io\notes\minimal-mod.min.css -Destination C:\Users\ashraaf\Downloads\VScode\hadithmv.github.io\app\webview-master\app\src\main\assets\notes\minimal-mod.min.css
+
+Copy-Item C:\Users\ashraaf\Downloads\VScode\hadithmv.github.io\notes\minimal-mod.min.css -Destination C:\Users\ashraaf\Downloads\VScode\hadithmv.github.io\win\hmv\notes\minimal-mod.min.css
