5c13782b16deeb6fee17c4f8cd3a77e37f848f13
yahyasdadmv@gmail.com <yahyasdadmv@gmail.com>
2021-01-11 03:21:22 +0500

    u
    Former-commit-id: 539cd0111c07ac43ee4b3c780487fc743dfe3b8d
    Former-commit-id: a078bc09ac27d5889bd2ee571200dc1efcad80d0

--- uc/minify-html.ps1
+++ uc/minify-html.ps1
@@ -1,1 +1,3 @@
 html-minifier --input-dir C:\Users\ashraaf\Downloads\VScode\hadithmv.github.io\uc --output-dir ..\ --file-ext html --collapse-boolean-attributes --collapse-whitespace --decode-entities --minify-css true --minify-js true --process-scripts [text/html] --remove-attribute-quotes --remove-comments --remove-empty-attributes --remove-optional-tags --remove-redundant-attributes --remove-script-type-attributes --remove-style-link-type-attributes --remove-tag-whitespace --sort-attributes --sort-class-name --trim-custom-fragments --use-short-doctype
+
+Copy-Item "C:\Users\ashraaf\Downloads\VScode\hadithmv.github.io\books\nawawi.html" -Destination "C:\Users\ashraaf\Downloads\VScode\hadithmv.github.io\_layouts\default.html"
