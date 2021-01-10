435e5861ed021b38454b4c3d6a8e7c9d946403fd
yahyasdadmv@gmail.com <yahyasdadmv@gmail.com>
2021-01-11 04:11:43 +0500

    u
    Former-commit-id: b6f8a3000d290e54ed5a5ec12bf37ba2162b0a8f

--- js/minify-js.ps1
+++ js/minify-js.ps1
@@ -1,8 +1,15 @@
 Set-Location -Path C:\Users\ashraaf\Downloads\VScode\hadithmv.github.io\js
+
 google-closure-compiler --charset=UTF-8 --js=barbahari-script.js --js_output_file=barbahari-script.min.js
+
 google-closure-compiler --charset=UTF-8 --js=nawawi-script.js --js_output_file=nawawi-script.min.js
+
 google-closure-compiler --charset=UTF-8 --js=otherHadith-script.js --js_output_file=otherHadith-script.min.js
+
 google-closure-compiler --charset=UTF-8 --js=quran-script.js --js_output_file=quran-script.min.js
+
 google-closure-compiler --charset=UTF-8 --js=radheef-script.js --js_output_file=radheef-script.min.js
+
 google-closure-compiler --charset=UTF-8 --js=sahihain-script.js --js_output_file=sahihain-script.js
+
 google-closure-compiler --charset=UTF-8 --js=umdah-script.js --js_output_file=umdah-script.min.js
---
+++ notes/info/minify-html.ps1
@@ -0,0 +1,22 @@
+Set-Location -Path C:\Users\ashraaf\Downloads\VScode\hadithmv.github.io\notes\info
+
+html-minifier --collapse-boolean-attributes --collapse-whitespace --decode-entities --minify-css true --minify-js true --process-scripts [text/html] --remove-attribute-quotes --remove-comments --remove-empty-attributes --remove-optional-tags --remove-redundant-attributes --remove-script-type-attributes --remove-style-link-type-attributes --remove-tag-whitespace --sort-attributes --sort-class-name --trim-custom-fragments --use-short-doctype introHmv-uc.html --output introHmv.html
+
+html-minifier --collapse-boolean-attributes --collapse-whitespace --decode-entities --minify-css true --minify-js true --process-scripts [text/html] --remove-attribute-quotes --remove-comments --remove-empty-attributes --remove-optional-tags --remove-redundant-attributes --remove-script-type-attributes --remove-style-link-type-attributes --remove-tag-whitespace --sort-attributes --sort-class-name --trim-custom-fragments --use-short-doctype introFortyN-uc.html --output introFortyN.html
+
+html-minifier --collapse-boolean-attributes --collapse-whitespace --decode-entities --minify-css true --minify-js true --process-scripts [text/html] --remove-attribute-quotes --remove-comments --remove-empty-attributes --remove-optional-tags --remove-redundant-attributes --remove-script-type-attributes --remove-style-link-type-attributes --remove-tag-whitespace --sort-attributes --sort-class-name --trim-custom-fragments --use-short-doctype introUmdah-uc.html --output introUmdah.html
+
+
+Copy-Item C:\Users\ashraaf\Downloads\VScode\hadithmv.github.io\notes\info\introHmv.html -Destination C:\Users\ashraaf\Downloads\VScode\hadithmv.github.io\app\webview-master\app\src\main\assets\notes\info\introHmv.html
+
+Copy-Item C:\Users\ashraaf\Downloads\VScode\hadithmv.github.io\notes\info\introFortyN.html -Destination C:\Users\ashraaf\Downloads\VScode\hadithmv.github.io\app\webview-master\app\src\main\assets\notes\info\introFortyN.html
+
+Copy-Item C:\Users\ashraaf\Downloads\VScode\hadithmv.github.io\notes\info\introUmdah.html -Destination C:\Users\ashraaf\Downloads\VScode\hadithmv.github.io\app\webview-master\app\src\main\assets\notes\info\introUmdah.html
+
+
+
+Copy-Item C:\Users\ashraaf\Downloads\VScode\hadithmv.github.io\notes\info\introHmv.html -Destination C:\Users\ashraaf\Downloads\VScode\hadithmv.github.io\win\hmv\notes\info\introHmv.html
+
+Copy-Item C:\Users\ashraaf\Downloads\VScode\hadithmv.github.io\notes\info\introFortyN.html -Destination C:\Users\ashraaf\Downloads\VScode\hadithmv.github.io\win\hmv\notes\info\introFortyN.html
+
+Copy-Item C:\Users\ashraaf\Downloads\VScode\hadithmv.github.io\notes\info\introUmdah.html -Destination C:\Users\ashraaf\Downloads\VScode\hadithmv.github.io\win\hmv\notes\info\introUmdah.html
