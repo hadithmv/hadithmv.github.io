a36cd5b2d90ea527be1f1739688800c1d472c64a
yahyasdadmv@gmail.com <yahyasdadmv@gmail.com>
2021-01-11 21:52:01 +0500

    u
    Former-commit-id: 3f52aee9feb86e571a9bd4b32469b4feb0c3005d

--- uc/old apps dir/minify-and-copy-html.ps1
+++ uc/old apps dir/minify-and-copy-html.ps1
@@ -3,4 +3,4 @@ html-minifier --input-dir C:\Users\ashraaf\Downloads\VScode\hadithmv.github.io\u
 
 html-minifier --input-dir C:\Users\ashraaf\Downloads\VScode\hadithmv.github.io\uc\apps --output-dir C:\Users\ashraaf\Downloads\VScode\hadithmv.github.io\win\hmv\books --file-ext html --collapse-boolean-attributes --collapse-whitespace --decode-entities --minify-css true --minify-js true --process-scripts [text/html] --remove-attribute-quotes --remove-comments --remove-empty-attributes --remove-optional-tags --remove-redundant-attributes --remove-script-type-attributes --remove-style-link-type-attributes --remove-tag-whitespace --sort-attributes --sort-class-name --trim-custom-fragments --use-short-doctype
 
-Move-Item -Path C:\Users\ashraaf\Downloads\VScode\hadithmv.github.io\win\hmv\books\index.html -Destination C:\Users\ashraaf\Downloads\VScode\hadithmv.github.io\win\hmv\index.html
+Move-Item -Path C:\Users\ashraaf\Downloads\VScode\hadithmv.github.io\win\hmv\books\index.html -Destination C:\Users\ashraaf\Downloads\VScode\hadithmv.github.io\win\hmv\index.html -force
