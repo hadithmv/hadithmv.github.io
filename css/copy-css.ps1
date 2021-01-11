3910263fdb7e12b855e5629eb83513a5efed0993
yahyasdadmv@gmail.com <yahyasdadmv@gmail.com>
2021-01-11 21:06:19 +0500

    u

---
+++ css/copy-css.ps1
@@ -0,0 +1,2 @@
+Copy-Item C:\Users\ashraaf\Downloads\VScode\hadithmv.github.io\css\COMBcss.min.css -Destination C:\Users\ashraaf\Downloads\VScode\hadithmv.github.io\app\webview-master\app\src\main\assets\css\COMBcss.min.css
+Copy-Item C:\Users\ashraaf\Downloads\VScode\hadithmv.github.io\css\COMBcss.min.css -Destination C:\Users\ashraaf\Downloads\VScode\hadithmv.github.io\win\hmv\css\COMBcss.min.css
