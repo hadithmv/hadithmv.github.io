5365302c9caaedd981952ba9368889e276559885
yahyasdadmv@gmail.com <yahyasdadmv@gmail.com>
2021-01-11 21:06:19 +0500

    u
    Former-commit-id: 3910263fdb7e12b855e5629eb83513a5efed0993

---
+++ js/copy-js.ps1
@@ -0,0 +1,21 @@
+Copy-Item C:\Users\ashraaf\Downloads\VScode\hadithmv.github.io\js\resc\COMBjs.js -Destination C:\Users\ashraaf\Downloads\VScode\hadithmv.github.io\app\webview-master\app\src\main\assets\js\resc\COMBjs.js
+Copy-Item C:\Users\ashraaf\Downloads\VScode\hadithmv.github.io\js\resc\COMBjs.js -Destination C:\Users\ashraaf\Downloads\VScode\hadithmv.github.io\win\hmv\js\resc\COMBjs.js
+
+
+Copy-Item C:\Users\ashraaf\Downloads\VScode\hadithmv.github.io\js\barbahari-script.min.js -Destination C:\Users\ashraaf\Downloads\VScode\hadithmv.github.io\app\webview-master\app\src\main\assets\js\barbahari-script.min.js
+Copy-Item C:\Users\ashraaf\Downloads\VScode\hadithmv.github.io\js\barbahari-script.min.js -Destination C:\Users\ashraaf\Downloads\VScode\hadithmv.github.io\win\hmv\js\barbahari-script.min.js
+
+Copy-Item C:\Users\ashraaf\Downloads\VScode\hadithmv.github.io\js\nawawi-script.min.js -Destination C:\Users\ashraaf\Downloads\VScode\hadithmv.github.io\app\webview-master\app\src\main\assets\js\nawawi-script.min.js
+Copy-Item C:\Users\ashraaf\Downloads\VScode\hadithmv.github.io\js\nawawi-script.min.js -Destination C:\Users\ashraaf\Downloads\VScode\hadithmv.github.io\win\hmv\js\nawawi-script.min.js
+
+Copy-Item C:\Users\ashraaf\Downloads\VScode\hadithmv.github.io\js\otherHadith-script.min.js -Destination C:\Users\ashraaf\Downloads\VScode\hadithmv.github.io\app\webview-master\app\src\main\assets\js\otherHadith-script.min.js
+Copy-Item C:\Users\ashraaf\Downloads\VScode\hadithmv.github.io\js\otherHadith-script.min.js -Destination C:\Users\ashraaf\Downloads\VScode\hadithmv.github.io\win\hmv\js\otherHadith-script.min.js
+
+Copy-Item C:\Users\ashraaf\Downloads\VScode\hadithmv.github.io\js\quran-script.min.js -Destination C:\Users\ashraaf\Downloads\VScode\hadithmv.github.io\app\webview-master\app\src\main\assets\js\quran-script.min.js
+Copy-Item C:\Users\ashraaf\Downloads\VScode\hadithmv.github.io\js\quran-script.min.js -Destination C:\Users\ashraaf\Downloads\VScode\hadithmv.github.io\win\hmv\js\quran-script.min.js
+
+Copy-Item C:\Users\ashraaf\Downloads\VScode\hadithmv.github.io\js\sahihain-script.min.js -Destination C:\Users\ashraaf\Downloads\VScode\hadithmv.github.io\app\webview-master\app\src\main\assets\js\sahihain-script.min.js
+Copy-Item C:\Users\ashraaf\Downloads\VScode\hadithmv.github.io\js\sahihain-script.min.js -Destination C:\Users\ashraaf\Downloads\VScode\hadithmv.github.io\win\hmv\js\sahihain-script.min.js
+
+Copy-Item C:\Users\ashraaf\Downloads\VScode\hadithmv.github.io\js\umdah-script.min.js -Destination C:\Users\ashraaf\Downloads\VScode\hadithmv.github.io\app\webview-master\app\src\main\assets\js\umdah-script.min.js
+Copy-Item C:\Users\ashraaf\Downloads\VScode\hadithmv.github.io\js\umdah-script.min.js -Destination C:\Users\ashraaf\Downloads\VScode\hadithmv.github.io\win\hmv\js\umdah-script.min.js
