665b8cb6277fdd3730131b18953a496edbcbc924
yahyasdadmv@gmail.com <yahyasdadmv@gmail.com>
2021-01-15 00:52:45 +0500

    u
    Former-commit-id: 660eaf8a2a1dc4dc3dfe30d57039f61d5abab133
    Former-commit-id: 231bdc5475e0e892333fc97564e522ae2610e4ca

---
+++ js/resc/copy-js.ps1
@@ -0,0 +1,3 @@
+Copy-Item C:\Users\ashraaf\Downloads\VScode\hadithmv.github.io\js\resc\COMBjs.js -Destination C:\Users\ashraaf\Downloads\VScode\hadithmv.github.io\app\webview-master\app\src\main\assets\js\resc\COMBjs.js
+
+Copy-Item C:\Users\ashraaf\Downloads\VScode\hadithmv.github.io\js\resc\COMBjs.js -Destination C:\Users\ashraaf\Downloads\VScode\hadithmv.github.io\win\hmv\js\resc\COMBjs.js
