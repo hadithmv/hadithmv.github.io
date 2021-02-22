37ff8831da7e230b63089b4a0feae4174182234d
yahyasdadmv@gmail.com <yahyasdadmv@gmail.com>
2021-02-22 13:10:17 +0500

    u
    Former-commit-id: 16d6871d805d55b6dfac46be5113bbfa3e9ffd09
    Former-commit-id: 2584a48f00c0de92bd11f46b6d378f551b46cb31

--- notes/minify-and-copy-css-and-thirdPartyLibs.ps1
+++ notes/minify-and-copy-css-and-thirdPartyLibs.ps1
@@ -4,6 +4,7 @@ Copy-Item C:\Users\ashraaf\Downloads\VScode\hadithmv.github.io\notes\minimal-mod
 
 Copy-Item C:\Users\ashraaf\Downloads\VScode\hadithmv.github.io\notes\minimal-mod.min.css -Destination C:\Users\ashraaf\Downloads\VScode\hadithmv.github.io\win\hmv\notes\minimal-mod.min.css
 
+
 Copy-Item C:\Users\ashraaf\Downloads\VScode\hadithmv.github.io\notes\third-party-libs-and-tools.txt -Destination C:\Users\ashraaf\Downloads\VScode\hadithmv.github.io\app\webview-master\app\src\main\assets\notes\third-party-libs-and-tools.txt
 
 Copy-Item C:\Users\ashraaf\Downloads\VScode\hadithmv.github.io\notes\third-party-libs-and-tools.txt -Destination C:\Users\ashraaf\Downloads\VScode\hadithmv.github.io\win\hmv\notes\third-party-libs-and-tools.txt
