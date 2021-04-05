9cccb66adf7f65d96ba688f5c2253208f57f2e31
yahyasdadmv@gmail.com <yahyasdadmv@gmail.com>
2021-04-05 11:42:00 +0500

    u
    Former-commit-id: 83db7317c505c3269d7c5a1b6ab35cbd350b9970
    Former-commit-id: e576317fd7294e9c1a9a967340b4ec9d52d20655

--- js/copy-js.ps1
+++ js/copy-js.ps1
@@ -1,7 +1,6 @@
 Copy-Item C:\Users\ashraaf\Downloads\VScode\hadithmv.github.io\js\resc\COMBjs.js -Destination C:\Users\ashraaf\Downloads\VScode\hadithmv.github.io\app\webview-master\app\src\main\assets\js\resc\COMBjs.js
 Copy-Item C:\Users\ashraaf\Downloads\VScode\hadithmv.github.io\js\resc\COMBjs.js -Destination C:\Users\ashraaf\Downloads\VScode\hadithmv.github.io\win\hmv\js\resc\COMBjs.js
 
-
 Copy-Item C:\Users\ashraaf\Downloads\VScode\hadithmv.github.io\js\barbahari-script.min.js -Destination C:\Users\ashraaf\Downloads\VScode\hadithmv.github.io\app\webview-master\app\src\main\assets\js\barbahari-script.min.js
 Copy-Item C:\Users\ashraaf\Downloads\VScode\hadithmv.github.io\js\barbahari-script.min.js -Destination C:\Users\ashraaf\Downloads\VScode\hadithmv.github.io\win\hmv\js\barbahari-script.min.js
 
