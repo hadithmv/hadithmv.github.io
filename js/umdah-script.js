0ea2d1244c2d4a5109492a1bbde8be11d629ac88
yahyasdadmv@gmail.com <yahyasdadmv@gmail.com>
2021-04-05 11:39:12 +0500

    u
    Former-commit-id: 75a496b273dfe79260bab20262cf03d149ad8553
    Former-commit-id: 905ee97a10e7505b495946af7be00a5ec8d847de

--- js/umdah-script.js
+++ js/umdah-script.js
@@ -346,7 +346,7 @@ $(document).ready(() => {
         extend: 'copy',
         key: { key: 'c', shiftKey: true },
         text: 'ކޮޕީ',
-        messageTop: 'ޙަދީޘްއެމްވީ - މްދަތުލް އަޙްކާމް', // CHANGE123 clipboard message
+        messageTop: 'ޙަދީޘްއެމްވީ - ޢުމްދަތުލް އަޙްކާމް', // CHANGE123 clipboard message
         title: '' /* title: "hadithmv.com", */,
 
         //= ====================
