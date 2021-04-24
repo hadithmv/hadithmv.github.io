f1bd59ca3ee8fccfa58cd89e2d428583ec419bdd
yahyasdadmv@gmail.com <yahyasdadmv@gmail.com>
2021-04-24 08:21:43 +0500

    u
    Former-commit-id: 0c0078e17e5093a9bd900de4780fc31246482af8
    Former-commit-id: 0061da455cf56b78cb6f4e1fe9815e5b98f43bdd

--- js/umdah-script.js
+++ js/umdah-script.js
@@ -306,7 +306,7 @@ $(document).ready(() => {
       infoEmpty: '- ނުފެނުނު -',
       lengthMenu: '_MENU_',
       search: '', // Originally "Search:" leave this blank in production
-      searchPlaceholder: 'ޢުމްދަތުލް އަޙްކާމިން ހޯއްދަވާ...',
+      searchPlaceholder: 'ހޯއްދަވާ...',
       zeroRecords: '- ނުފެނުނު -'
     }, //= =================== End of Internationalisation
 
