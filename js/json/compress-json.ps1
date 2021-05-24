cb510d66a496e782f8cfe83af5af0f2983dfde98
yahyasdadmv@gmail.com <yahyasdadmv@gmail.com>
2021-05-24 18:12:54 +0500

    u

---
+++ js/json/compress-json.ps1
@@ -0,0 +1,7 @@
+uglifyjs barbahari.js -c -m -o barbahari.js
+uglifyjs eegaal.js -c -m -o eegaal.js
+uglifyjs otherHadith.js -c -m -o otherHadith.js
+uglifyjs quran.js -c -m -o quran.js
+uglifyjs radheef.js -c -m -o radheef.js
+uglifyjs sahihain.js -c -m -o sahihain.js
+uglifyjs umdah.js -c -m -o umdah.js
