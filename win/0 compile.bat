e49c7c529e3e81dbebc1358468f00fa48ced23ee
yahyasdadmv@gmail.com <yahyasdadmv@gmail.com>
2021-06-01 22:35:45 +0500

    u
    Former-commit-id: 19c1e61df98d77787d047cebbb8309c8e8a29e89
    Former-commit-id: f07a433e0ec61102e7504b4455057f2f5a6e444c

---
+++ win/0 compile.bat
@@ -0,0 +1,15 @@
+﻿@ECHO OFF
+CLS
+
+REM Set our app file in APP variable
+SET APP="C:\Users\ashraaf\Downloads\VScode\hadithmv.github.io\win\hadithmv.dhc"
+
+REM Set Compiler CLI path
+SET COMPILER="C:\Program Files\DecSoft\HtmlCompiler\HtmlCompilerCLI.exe"
+
+REM Call the compiler with the app file
+%COMPILER% %APP%
+
+REM Pause the script execution to see the results
+PAUSE
+
