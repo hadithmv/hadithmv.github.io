90c1899744eaf461e086abb76bf54e54fc16cc98
yahyasdadmv@gmail.com <yahyasdadmv@gmail.com>
2021-06-04 19:50:34 +0500

    u
    Former-commit-id: a8be34c1565f7154c4e629b91e5b3ebde4b78d04
    Former-commit-id: 6f9651e5a311107e323cb92d46c9d4fcdc9a5f7f

--- win/0 compile-zip-delete.ps1
+++ win/0 compile-zip-delete.ps1
@@ -6,7 +6,7 @@ C:\Users\ashraaf\Downloads\VScode\hadithmv.github.io\win\compile.bat
 
 # 7za  -tzip <archive-name> <folder-name>
 # level of compression = 9 (Ultra)
-C:\Users\ashraaf\Downloads\VScode\hadithmv.github.io\win\7za.exe a -mx=9 "C:\Users\ashraaf\Downloads\VScode\hadithmv.github.io\win\hmv\hadithmv_Win64.7z" "C:\Users\ashraaf\Downloads\VScode\hadithmv.github.io\win\hmv\hadithmv_Win64.exe"
+C:\Users\ashraaf\Downloads\7z1900-extra\x64\7za.exe a -mx=9 "C:\Users\ashraaf\Downloads\VScode\hadithmv.github.io\win\hmv\hadithmv_Win64.7z" "C:\Users\ashraaf\Downloads\VScode\hadithmv.github.io\win\hmv\hadithmv_Win64.exe"
 
 # delete exe to avoid binary file warning from github
 Remove-Item "C:\Users\ashraaf\Downloads\VScode\hadithmv.github.io\win\hmv\hadithmv_Win64.exe"
