52321ca4eb3cbb150e72e0842c11f8c65eec8b20
yahyasdadmv@gmail.com <yahyasdadmv@gmail.com>
2021-07-06 19:20:16 +0500

    u
    Former-commit-id: ec47f294aa70afe5338d8d9d393180ef40db121d
    Former-commit-id: 7fbd1f39f6cdd4babedd754f7d49dfae3b9b9ee3

--- js/quran-script.js
+++ js/quran-script.js
@@ -686,11 +686,10 @@ $(document).ready(() => {
   })
 
   // removes diacritics on key up
-  $('.dataTables_filter input').off().on('keyup', function () {
+  /*$('.dataTables_filter input').off().on('keyup', function () {
     let str = $(this).val()
-    str = str.replace(/ّ/g, '')
-    str = str.replace(/[ًٌٍَُِْ]/g, '')
+    str = str.replace(/[ًٌٍَُِّْ]/g, '')
     table.search(str).draw()
-  })
+  }) */
   //
 }) // ==================== END OF $(document).ready( function () {
