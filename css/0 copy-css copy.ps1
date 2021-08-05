925f7258ed59ba082f896dd337abf7eec6428f90
yahyasdadmv@gmail.com <yahyasdadmv@gmail.com>
2021-08-05 21:35:56 +0500

    u
    Former-commit-id: 7dd9d37af1406d368a92de33111d93d886adceaf

--- css/0 copy-css copy.ps1
+++
@@ -1,3 +0,0 @@
-Copy-Item C:\Users\ashraaf\Downloads\VScode\hadithmv.github.io\css\COMB-nonCrit.min.css -Destination C:\Users\ashraaf\Downloads\VScode\hadithmv.github.io\app\webview-master\app\src\main\assets\css\COMB-nonCrit.min.css
-
-Copy-Item C:\Users\ashraaf\Downloads\VScode\hadithmv.github.io\css\COMB-nonCrit.min.css -Destination C:\Users\ashraaf\Downloads\VScode\hadithmv.github.io\win\hmv\css\COMB-nonCrit.min.css
