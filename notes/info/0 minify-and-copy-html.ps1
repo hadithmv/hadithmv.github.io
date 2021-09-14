a01d080eba9ac8902e3d1670a6e86f4876326c71
yahyasdadmv@gmail.com <yahyasdadmv@gmail.com>
2021-09-15 00:17:49 +0300

    u
    Former-commit-id: 3c2e89c1f801491ce35bc2e8c120d392dc7c5f5a
    Former-commit-id: d553d0b2a8fe3247e386e7f275533ec324ae1041
    Former-commit-id: 5fa113440b5f521f80521539a8f0209a05ec89d8

--- notes/info/0 minify-and-copy-html.ps1
+++ notes/info/0 minify-and-copy-html.ps1
@@ -8,6 +8,8 @@ html-minifier --collapse-boolean-attributes --collapse-whitespace --decode-entit
 
 html-minifier --collapse-boolean-attributes --collapse-whitespace --decode-entities --minify-css true --minify-js true --process-scripts [text/html] --remove-attribute-quotes --remove-comments --remove-empty-attributes --remove-optional-tags --remove-redundant-attributes --remove-script-type-attributes --remove-style-link-type-attributes --remove-tag-whitespace --sort-attributes --sort-class-name --trim-custom-fragments --use-short-doctype shop-uc.html -o ../../shop.html
 
+html-minifier --collapse-boolean-attributes --collapse-whitespace --decode-entities --minify-css true --minify-js true --process-scripts [text/html] --remove-attribute-quotes --remove-comments --remove-empty-attributes --remove-optional-tags --remove-redundant-attributes --remove-script-type-attributes --remove-style-link-type-attributes --remove-tag-whitespace --sort-attributes --sort-class-name --trim-custom-fragments --use-short-doctype FAQ-uc.html -o ../../FAQ.html
+
 
 
 
@@ -17,6 +19,8 @@ Copy-Item introFortyN.html -Destination ../../app/webview-master/app/src/main/as
 
 Copy-Item introUmdah.html -Destination ../../app/webview-master/app/src/main/assets/notes/info/introUmdah.html
 
+Copy-Item FAQ.html -Destination ../../app/webview-master/app/src/main/assets/notes/info/FAQ.html
+
 
 
 
@@ -25,3 +29,5 @@ Copy-Item introHmv.html -Destination ../../win/hmv/notes/info/introHmv.html
 Copy-Item introFortyN.html -Destination ../../win/hmv/notes/info/introFortyN.html
 
 Copy-Item introUmdah.html -Destination ../../win/hmv/notes/info/introUmdah.html
+
+Copy-Item FAQ.html -Destination ../../win/hmv/notes/info/FAQ.html
