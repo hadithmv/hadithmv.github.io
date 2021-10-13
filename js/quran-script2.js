681eaeaea02ca806a40c8b4e27e4ab9a5a744e01
yahyasdadmv@gmail.com <yahyasdadmv@gmail.com>
2021-10-13 22:44:57 +0300

    u
    Former-commit-id: 7ffeda75ce6b888507da7939bf44cb73785c7528
    Former-commit-id: ec3e063022167646df6163701ae48fbb3b82a886
    Former-commit-id: 88119568d350743f086fdae66104cea15ac8a588

--- js/quran-script2.js
+++ js/quran-script2.js
@@ -91,7 +91,7 @@ $(document).ready(() => {
           https://stackoverflow.com/questions/2939766/regex-to-reverse-order-of-list
           https://www.fileformat.info/info/unicode/char/202e/index.htm */
           // data = data.replace(/([\u0660-\u0669]+)([\u0660-\u0669]+)([\u0660-\u0669]+)/, '$3$2$1')
-          data = data.replace(/([\u0660-\u0669]+)/, '\u202E$1')
+          data = data.replace(/([\u0660-\u0669]+)/, '\u202E$1\u202D')
           /* reverse brackets because thats how the font file needs it */
           data = '﴿' + data + '﴾'
           /* move the bracket in surah start basmalas to the actual first ayah */
@@ -460,7 +460,7 @@ $(document).ready(() => {
           // data = data.replace(/\r\n\r\n/g, ' ') //  ONLY FOR 40N, because it has an inserted title
           // data = data.replace(/\n\n/g, ' ') //  ONLY FOR 40N, because it has an inserted title, the two lines below arent needed for 40n i think
 
-          data = data.replace(/([\u0660-\u0669]+)/, '\u202E$1\u202c')
+          data = data.replace(/([\u0660-\u0669]+)/, '\u202E$1\u202D')
           
           data = data.replace(/\r\n/g, '\t') //  prevents first header showing up unneeded (windows)
           data = data.replace(/\n/g, '\t') //  prevents first header showing up unneeded (linux) this needs to go below windows rn
