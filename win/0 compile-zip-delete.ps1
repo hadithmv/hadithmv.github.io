83b84a72538495938ee4cd36d74b751216d41c0c
yahyasdadmv@gmail.com <yahyasdadmv@gmail.com>
2021-10-15 19:36:20 +0300

    u
    Former-commit-id: 048d6a26a8c308277cc2adfe373240e888537b9a
    Former-commit-id: 83f73c8fbf743ba69c366c9fa0eb9295b6079fb8

--- win/0 compile-zip-delete.ps1
+++ win/0 compile-zip-delete.ps1
@@ -1,3 +1,38 @@
+#
+# COPY BOOKS
+#
+
+# copies site minified book files to asset book files one by one
+Copy-Item ../books/40nawawi.html -Destination ../app/webview-master/app/src/main/assets/books/40nawawi.html
+
+Copy-Item ../books/barbahari.html -Destination ../app/webview-master/app/src/main/assets/books/barbahari.html
+
+Copy-Item ../books/eegaal.html -Destination ../app/webview-master/app/src/main/assets/books/eegaal.html
+
+Copy-Item ../books/index.html -Destination ../app/webview-master/app/src/main/assets/books/index.html
+
+Copy-Item ../books/quran.html -Destination ../app/webview-master/app/src/main/assets/books/quran.html
+
+Copy-Item ../books/radheef.html -Destination ../app/webview-master/app/src/main/assets/books/radheef.html
+
+Copy-Item ../books/umdah.html -Destination ../app/webview-master/app/src/main/assets/books/umdah.html
+
+Copy-Item ../books/allHadith.html -Destination ../app/webview-master/app/src/main/assets/books/allHadith.html
+
+Copy-Item ../books/hisnulMuslim.html -Destination ../app/webview-master/app/src/main/assets/books/hisnulMuslim.html
+
+# copies minified asset book folder contents to win book folder
+Copy-Item ../app/webview-master/app/src/main/assets/books/* -Destination ../win/hmv/books
+
+# copies index from win book to win root
+Copy-Item ../win/hmv/books/index.html -Destination ../win/hmv/index.html
+
+
+#
+# WIN ONLY COMPILE
+#
+
+
 # delete old 7z archive because that way new one is smaller
 Remove-Item "C:\Users\ashraaf\Downloads\VScode\hadithmv.github.io\win\hmv\hadithmv_Win64.7z"
 
