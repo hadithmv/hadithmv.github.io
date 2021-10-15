2d5096476e68130c4d3e564998feede8ecabd886
yahyasdadmv@gmail.com <yahyasdadmv@gmail.com>
2021-10-15 19:36:20 +0300

    u
    Former-commit-id: 048d6a26a8c308277cc2adfe373240e888537b9a
    Former-commit-id: 83f73c8fbf743ba69c366c9fa0eb9295b6079fb8
    Former-commit-id: 83b84a72538495938ee4cd36d74b751216d41c0c

--- uc/00 minify-and-copy-html.ps1
+++ uc/00 minify-and-copy-html.ps1
@@ -5,30 +5,3 @@ html-minifier --input-dir $PSScriptRoot --output-dir ../books --file-ext html --
 
 # copies nawawi over to layout index
 Copy-Item ../books/index.html -Destination ../_layouts/index.html
-
-
-
-# copies site minified book files to asset book files one by one
-Copy-Item ../books/40nawawi.html -Destination ../app/webview-master/app/src/main/assets/books/40nawawi.html
-
-Copy-Item ../books/barbahari.html -Destination ../app/webview-master/app/src/main/assets/books/barbahari.html
-
-Copy-Item ../books/eegaal.html -Destination ../app/webview-master/app/src/main/assets/books/eegaal.html
-
-Copy-Item ../books/index.html -Destination ../app/webview-master/app/src/main/assets/books/index.html
-
-Copy-Item ../books/quran.html -Destination ../app/webview-master/app/src/main/assets/books/quran.html
-
-Copy-Item ../books/radheef.html -Destination ../app/webview-master/app/src/main/assets/books/radheef.html
-
-Copy-Item ../books/umdah.html -Destination ../app/webview-master/app/src/main/assets/books/umdah.html
-
-Copy-Item ../books/allHadith.html -Destination ../app/webview-master/app/src/main/assets/books/allHadith.html
-
-Copy-Item ../books/hisnulMuslim.html -Destination ../app/webview-master/app/src/main/assets/books/hisnulMuslim.html
-
-# copies minified asset book folder contents to win book folder
-Copy-Item -Path ../app/webview-master/app/src/main/assets/books/* -Destination ../win/hmv/books
-
-# copies index from win book to win root
-Copy-Item ../win/hmv/books/index.html -Destination ../win/hmv/index.html
