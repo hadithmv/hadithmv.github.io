5fb7de576fb9e060e6aaa0c6e0c419c5d8e7ff29
yahyasdadmv@gmail.com <yahyasdadmv@gmail.com>
2021-10-17 03:11:38 +0300

    u
    Former-commit-id: 19faaa0cbaac79a88ee39dcc0edf33819a99baa6

--- notes/info/0 minify-and-copy-html.ps1
+++ notes/info/0 minify-and-copy-html.ps1
@@ -11,29 +11,3 @@ html-minifier --collapse-boolean-attributes --collapse-whitespace --decode-entit
 html-minifier --collapse-boolean-attributes --collapse-whitespace --decode-entities --minify-css true --minify-js true --process-scripts [text/html] --remove-attribute-quotes --remove-comments --remove-empty-attributes --remove-optional-tags --remove-redundant-attributes --remove-script-type-attributes --remove-style-link-type-attributes --remove-tag-whitespace --sort-attributes --sort-class-name --trim-custom-fragments --use-short-doctype FAQ-uc.html -o FAQ.html
 
 html-minifier --collapse-boolean-attributes --collapse-whitespace --decode-entities --minify-css true --minify-js true --process-scripts [text/html] --remove-attribute-quotes --remove-comments --remove-empty-attributes --remove-optional-tags --remove-redundant-attributes --remove-script-type-attributes --remove-style-link-type-attributes --remove-tag-whitespace --sort-attributes --sort-class-name --trim-custom-fragments --use-short-doctype contributors-uc.html -o contributors.html
-
-
-
-
-Copy-Item introHmv.html -Destination ../../app/webview-master/app/src/main/assets/notes/info/introHmv.html
-
-Copy-Item introFortyN.html -Destination ../../app/webview-master/app/src/main/assets/notes/info/introFortyN.html
-
-Copy-Item introUmdah.html -Destination ../../app/webview-master/app/src/main/assets/notes/info/introUmdah.html
-
-Copy-Item FAQ.html -Destination ../../app/webview-master/app/src/main/assets/notes/info/FAQ.html
-
-Copy-Item contributors.html -Destination ../../app/webview-master/app/src/main/assets/notes/info/contributors.html
-
-
-
-
-Copy-Item introHmv.html -Destination ../../win/hmv/notes/info/introHmv.html
-
-Copy-Item introFortyN.html -Destination ../../win/hmv/notes/info/introFortyN.html
-
-Copy-Item introUmdah.html -Destination ../../win/hmv/notes/info/introUmdah.html
-
-Copy-Item FAQ.html -Destination ../../win/hmv/notes/info/FAQ.html
-
-Copy-Item contributors.html -Destination ../../win/hmv/notes/info/contributors.html
