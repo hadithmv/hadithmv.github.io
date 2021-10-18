ec47eaf7219abcb998ea17ce4e50d56bb64ecf18
yahyasdadmv@gmail.com <yahyasdadmv@gmail.com>
2021-10-18 03:29:56 +0300

    u
    Former-commit-id: 1dd4c53ef414d3e1d642a126ce642f8c0d9e73c5

--- js/json/0 minify-and-copy-json.ps1
+++ js/json/0 minify-and-copy-json.ps1
@@ -11,30 +11,3 @@ uglifyjs umdah.js -c -m -o umdah.js
 uglifyjs bulugh.js -c -m -o bulugh.js
 uglifyjs allHadith.js -c -m -o allHadith.js
 uglifyjs hisnulMuslim.js -c -m -o hisnulMuslim.js
-
-
-# copy
-
-Copy-Item barbahari.js -Destination ../../app/webview-master/app/src/main/assets/js/json/barbahari.js
-Copy-Item barbahari.js -Destination ../../win/hmv/js/json/barbahari.js
-
-Copy-Item nawawi.js -Destination ../../app/webview-master/app/src/main/assets/js/json/nawawi.js
-Copy-Item nawawi.js -Destination ../../win/hmv/js/json/nawawi.js
-
-Copy-Item quran.js -Destination ../../app/webview-master/app/src/main/assets/js/json/quran.js
-Copy-Item quran.js -Destination ../../win/hmv/js/json/quran.js
-
-Copy-Item umdah.js -Destination ../../app/webview-master/app/src/main/assets/js/json/umdah.js
-Copy-Item umdah.js -Destination ../../win/hmv/js/json/umdah.js
-
-Copy-Item radheef.js -Destination ../../app/webview-master/app/src/main/assets/js/json/radheef.js
-Copy-Item radheef.js -Destination ../../win/hmv/js/json/radheef.js
-
-Copy-Item eegaal.js -Destination ../../app/webview-master/app/src/main/assets/js/json/eegaal.js
-Copy-Item eegaal.js -Destination ../../win/hmv/js/json/eegaal.js
-
-Copy-Item allHadith.js -Destination ../../app/webview-master/app/src/main/assets/js/json/allHadith.js
-Copy-Item allHadith.js -Destination ../../win/hmv/js/json/allHadith.js
-
-Copy-Item hisnulMuslim.js -Destination ../../app/webview-master/app/src/main/assets/js/json/hisnulMuslim.js
-Copy-Item hisnulMuslim.js -Destination ../../win/hmv/js/json/hisnulMuslim.js
