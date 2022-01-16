609f11841346fde41ff03f32932bca00da051bdb
yahyasdadmv@gmail.com <yahyasdadmv@gmail.com>
2022-01-17 00:31:02 +0300

    u
    Former-commit-id: 973248258512fd015a183335eb8ecdc1c4eaae39

--- js/json/0 minify-json.ps1
+++ js/json/0 minify-json.ps1
@@ -13,3 +13,4 @@ uglifyjs allHadith.js -c -m -o allHadith.js
 uglifyjs hisnulMuslim.js -c -m -o hisnulMuslim.js
 uglifyjs aajurry.js -c -m -o aajurry.js
 uglifyjs muharrar.js -c -m -o muharrar.js
+uglifyjs muwatta.js -c -m -o muwatta.js
