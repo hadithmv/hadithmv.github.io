96ae77878d9baf169ca96a755a65b0b7f5333049
yahyasdadmv@gmail.com <yahyasdadmv@gmail.com>
2022-02-05 18:16:22 +0300

    u
    Former-commit-id: 6596cf439449abda90c49a98bfbfa25c7da3ef12

--- uc/00 minify-and-copy-html.ps1
+++ uc/00 minify-and-copy-html.ps1
@@ -24,6 +24,7 @@ html-minifier --collapse-boolean-attributes --collapse-whitespace --decode-entit
 
 html-minifier --collapse-boolean-attributes --collapse-whitespace --decode-entities --minify-css true --minify-js true --process-scripts [text/html] --remove-attribute-quotes --remove-comments --remove-empty-attributes --remove-optional-tags --remove-redundant-attributes --remove-script-type-attributes --remove-style-link-type-attributes --remove-tag-whitespace --sort-attributes --sort-class-name --trim-custom-fragments --use-short-doctype umdah.html -o ../books/umdah.html
 
+html-minifier --collapse-boolean-attributes --collapse-whitespace --decode-entities --minify-css true --minify-js true --process-scripts [text/html] --remove-attribute-quotes --remove-comments --remove-empty-attributes --remove-optional-tags --remove-redundant-attributes --remove-script-type-attributes --remove-style-link-type-attributes --remove-tag-whitespace --sort-attributes --sort-class-name --trim-custom-fragments --use-short-doctype muwatta.html -o ../books/muwatta.html
 
 # copies nawawi over to layout index
 Copy-Item ../books/index.html -Destination ../_layouts/index.html
