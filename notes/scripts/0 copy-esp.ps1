0f0c96559766a045445dc242669589e8c2017674
hadithmv <yahyasdadmv@gmail.com>
2022-02-09 01:35:55 +0300

    u
    Former-commit-id: 14aec637ed1a064d2b7986c7be60e90d34584d42

--- notes/scripts/0 copy-esp.ps1
+++ notes/scripts/0 copy-esp.ps1
@@ -1,1 +1,5 @@
-Copy-Item C:\Users\ashraaf\Downloads\VScode\hadithmv.github.io\notes\scripts\default.yml -Destination C:\Users\ashraaf\AppData\Roaming\espanso\default.yml
+#Copy-Item C:\Users\ashraaf\Downloads\VScode\hadithmv.github.io\notes\scripts\default.yml -Destination C:\Users\ashraaf\AppData\Roaming\espanso\default.yml
+
+Set-Location -Path $PSScriptRoot #C:/Users/ashraaf/Downloads/VScode/hadithmv.github.io/uc
+
+Copy-Item default.yml -Destination C:\Users\hadit\AppData\Roaming\espanso
