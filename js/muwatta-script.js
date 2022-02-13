0c85418fc2b3a45fe39545a057d12c5e6c2b88ff
hadithmv <yahyasdadmv@gmail.com>
2022-02-13 17:45:10 +0300

    u
    Former-commit-id: e9f70ce8142d324c639e7984f6cdf0a71a6d46cf

--- js/muwatta-script.js
+++ js/muwatta-script.js
@@ -402,7 +402,7 @@ $(document).ready(() => {
         extend: 'copy',
         key: { key: 'c', shiftKey: true },
         text: 'ކޮޕީ',
-        messageTop: 'ޙަދީޘްއެމްވީ - ބުލޫޣުލް މަރާމް', // CHANGE123 clipboard message
+        messageTop: 'ޙަދީޘްއެމްވީ - މުވައްޠަ މާލިކު', // CHANGE123 clipboard message
         title: '' /* title: "hadithmv.com", */,
 
         //= ====================
