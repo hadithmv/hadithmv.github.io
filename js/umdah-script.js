a12f947930db7eb6782c52df9c67a819cae6cb22
hadithmv <yahyasdadmv@gmail.com>
2022-02-24 03:50:41 +0300

    u
    Former-commit-id: 955eca40e58a0ed50ad008e3696ee711e6a08263
    Former-commit-id: 520436813a2d7a94fea2e56fa0009092f507bbca

--- js/umdah-script.js
+++ js/umdah-script.js
@@ -123,7 +123,7 @@ $(document).ready(() => {
       // adds footnote line for shurooh
       { targets: 8,
         render: function (data, type, row) {
-          data = '_______________<br>' + data
+          data = '____________________<br>' + data
           return data.replace(/\n/g, ' <br class="br">') // without this line breaks not preserved
           } 
         },
@@ -439,7 +439,7 @@ $(document).ready(() => {
           data = data.replace(/އަރަބި ފިލިނުޖަހައި\t/g, '')
           data = data.replace(/ދިވެހި ތަރުޖަމާ\t/g, '')
           data = data.replace(/ތަޚްރީޖު\t/g, '')
-          data = data.replace(/\t_______________/g, '\n\n_______________\n\n') // adds a line break after takhri line
+          data = data.replace(/\t____________________/g, '\n\n____________________\n\n') // adds a line break after takhri line
 
           data = data.replace(/\t\t/g, '\t') //  This prevents a double or more line breaks when columns are hidden
           data = data.replace(/\t/g, '\n\n') // creates line breaks between cell data
