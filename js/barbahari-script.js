2d5775091e17052ad3f2130a019c4417243219e2
hadithmv <yahyasdadmv@gmail.com>
2022-03-11 09:14:57 +0300

    u
    Former-commit-id: 382e429a85d389bc3d52332fac727436817cb7a9
    Former-commit-id: bb114484f32607e03f6d2fb684a2cf8926d179f0

--- js/barbahari-script.js
+++ js/barbahari-script.js
@@ -380,7 +380,7 @@ $(document).ready(() => {
 
           data = data.replace(/\t\t/g, '\t') //  This prevents a double or more line breaks when columns are hidden
           data = data.replace(/\t/g, '\n\n') // creates line breaks between cell data
-          //data = data.replace(/\s\s/g, '\n\n') // turns two spaces into new lines, for multi line text
+          data = data.replace(/\s\s/g, '\n\n') // turns two spaces into new lines, for multi line text
           data = data.replace(/\n\n\n\n/g, '\n\n') // turns 4 new lines spaces into 2
           data = data.replace(/\n\n\n\n/g, '\n\n') // turns 4 new lines spaces into 2
 
