e85b1057e938f62028b13ed17c3893f1a14662f0
hadithmv <yahyasdadmv@gmail.com>
2022-03-14 03:10:16 +0300

    u
    Former-commit-id: 177ab0ec7d92cc0483fb0424cd3e0709d5a814b7

--- uc/00 minify-and-copy-html.ps1
+++ uc/00 minify-and-copy-html.ps1
@@ -26,5 +26,7 @@ html-minifier --collapse-boolean-attributes --collapse-whitespace --decode-entit
 
 html-minifier --collapse-boolean-attributes --collapse-whitespace --decode-entities --minify-css true --minify-js true --process-scripts [text/html] --remove-attribute-quotes --remove-comments --remove-empty-attributes --remove-optional-tags --remove-redundant-attributes --remove-script-type-attributes --remove-style-link-type-attributes --remove-tag-whitespace --sort-attributes --sort-class-name --trim-custom-fragments --use-short-doctype muwatta.html -o ../books/muwatta.html
 
+html-minifier --collapse-boolean-attributes --collapse-whitespace --decode-entities --minify-css true --minify-js true --process-scripts [text/html] --remove-attribute-quotes --remove-comments --remove-empty-attributes --remove-optional-tags --remove-redundant-attributes --remove-script-type-attributes --remove-style-link-type-attributes --remove-tag-whitespace --sort-attributes --sort-class-name --trim-custom-fragments --use-short-doctype nawaqidulislam.html -o ../books/nawaqidulislam.html
+
 # copies nawawi over to layout index
 Copy-Item ../books/index.html -Destination ../_layouts/index.html
