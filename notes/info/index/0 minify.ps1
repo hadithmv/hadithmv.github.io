0134dab561b101374146c0a21de4653daada6aa7
hadithmv <yahyasdadmv@gmail.com>
2022-03-20 20:15:27 +0300

    u
    Former-commit-id: 497972dcd717b23e93d89e53f70fafab09891159
    Former-commit-id: 63ce34296c77cd37dfc39411f59ae6300324f3d4

---
+++ notes/info/index/0 minify.ps1
@@ -0,0 +1,3 @@
+Set-Location -Path $PSScriptRoot #C:/Users/ashraaf/Downloads/VScode/hadithmv.github.io/notes/info
+
+html-minifier --collapse-boolean-attributes --collapse-whitespace --decode-entities --minify-css true --minify-js true --process-scripts [text/html] --remove-attribute-quotes --remove-comments --remove-empty-attributes --remove-optional-tags --remove-redundant-attributes --remove-script-type-attributes --remove-style-link-type-attributes --remove-tag-whitespace --sort-attributes --sort-class-name --trim-custom-fragments --use-short-doctype index-uc.html -o index.html
