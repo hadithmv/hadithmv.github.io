fcdc33d57c6fd1d3fe26930f0aa348d5ec8a1507
hadithmv <yahyasdadmv@gmail.com>
2022-04-01 08:09:03 +0300

    u

--- js/quranBakurube-script.js
+++ js/quranBakurube-script.js
@@ -717,7 +717,7 @@ $(document).ready(() => {
     $('.dataTable').on('page.dt', () => {
       $('html, body').animate(
         {
-          scrollTop: 140 //prev 0
+          scrollTop: 145 //prev 0
         },
         'fast'
       )
