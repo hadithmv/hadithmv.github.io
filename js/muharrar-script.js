5aa30089a6124868594b0a0f11c12efca8521bfc
hadithmv <yahyasdadmv@gmail.com>
2022-04-01 07:25:59 +0300

    u
    Former-commit-id: 62a4584ff93319d6c3e3743fa0a0a7c1df871255
    Former-commit-id: f8ad51e430e63dcd1c1bd5232cc96e7f61a01810

--- js/muharrar-script.js
+++ js/muharrar-script.js
@@ -587,7 +587,7 @@ $(document).ready(() => {
     $('.dataTable').on('page.dt', () => {
       $('html, body').animate(
         {
-          scrollTop: 160 //prev 0
+          scrollTop: 140 //prev 0
         },
         'fast'
       )
