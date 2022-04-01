6416526efe0a39c96ae85e42ccad04d56649c69a
hadithmv <yahyasdadmv@gmail.com>
2022-04-01 06:46:02 +0300

    u

--- js/aajurry-script.js
+++ js/aajurry-script.js
@@ -635,7 +635,7 @@ $(document).ready(() => {
     $('.dataTable').on('page.dt', () => {
       $('html, body').animate(
         {
-          scrollTop: 150 //prev 0
+          scrollTop: 160 //prev 0
         },
         'fast'
       )
