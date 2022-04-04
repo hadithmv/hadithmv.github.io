db0d018a4c00e79432bc4162b9b0f64bd692e635
hadithmv <yahyasdadmv@gmail.com>
2022-04-04 16:01:30 +0300

    u
    Former-commit-id: 3d7c67cf45e32213e33a8f877f7cd42466c288ae
    Former-commit-id: 885714310c80512d5e8275bfed85b061a48129df

--- win/2 compile win.ps1
+++ win/2 compile win.ps1
@@ -10,7 +10,7 @@ C:\Users\hadit\Downloads\VScode\hadithmv.github.io\win\compile.bat
 
 # 7za  -tzip <archive-name> <folder-name>
 # level of compression = 9 (Ultra)
-C:\Users\hadit\Downloads\VScode\hadithmv.github.io\win\7z2106-extra\x64\7za.exe a -mx=9 "C:\Users\hadit\Desktop\hadithmv_Win64.7z" "C:\Users\hadit\Downloads\VScode\hadithmv.github.io\win\hmv\hadithmv_Win64.exe"
+C:\Users\hadit\Downloads\7z2107-extra\7za.exe a -mx=9 "C:\Users\hadit\Desktop\hadithmv_Win64.7z" "C:\Users\hadit\Downloads\VScode\hadithmv.github.io\win\hmv\hadithmv_Win64.exe"
 
 # delete exe to avoid binary file warning from github
 Remove-Item "C:\Users\hadit\Downloads\VScode\hadithmv.github.io\win\hmv\hadithmv_Win64.exe"
