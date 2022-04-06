d54bf46c7ba2a758b0794660b053ef9e4118c46a
hadithmv <yahyasdadmv@gmail.com>
2022-04-06 17:42:56 +0300

    u
    Former-commit-id: 2c13cdbd2e13fe8ca56f81f608ce1f40728c2c66
    Former-commit-id: 61b0ffaf9fc9c2cc5e0de32659e19dbaf1b32afc

--- js/json/0 minify-json.ps1
+++ js/json/0 minify-json.ps1
@@ -2,18 +2,17 @@ Set-Location -Path $PSScriptRoot
 
 #minify the spaces
 
-uglifyjs nawawi.js -c -m -o nawawi.js
-uglifyjs barbahari.js -c -m -o barbahari.js
-uglifyjs eegaal.js -c -m -o eegaal.js
-uglifyjs quran.js -c -m -o quran.js
-uglifyjs radheef.js -c -m -o radheef.js
-uglifyjs umdah.js -c -m -o umdah.js
-uglifyjs bulugh.js -c -m -o bulugh.js
 uglifyjs allHadith.js -c -m -o allHadith.js
+uglifyjs barbahari.js -c -m -o barbahari.js
+uglifyjs bulugh.js -c -m -o bulugh.js
+uglifyjs eegaal.js -c -m -o eegaal.js
 uglifyjs hisnulMuslim.js -c -m -o hisnulMuslim.js
-uglifyjs aajurry.js -c -m -o aajurry.js
-uglifyjs muharrar.js -c -m -o muharrar.js
 uglifyjs muwatta.js -c -m -o muwatta.js
 uglifyjs nawaqid.js -c -m -o nawaqid.js
+uglifyjs nawawi.js -c -m -o nawawi.js
 uglifyjs qawaid.js -c -m -o qawaid.js
+uglifyjs quran.js -c -m -o quran.js
 uglifyjs quranBakurube.js -c -m -o quranBakurube.js
+uglifyjs radheef.js -c -m -o radheef.js
+uglifyjs raziyain.js -c -m -o raziyain.js
+uglifyjs umdah.js -c -m -o umdah.js
