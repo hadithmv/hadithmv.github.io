c66a1668507ccdf56ea4bf2cd6bbf68972dcaac0
hadithmv <yahyasdadmv@gmail.com>
2022-07-29 03:21:22 +0500

    u
    Former-commit-id: 9a85fe2d0bdc36ea7a9f291c2294a4c3830a47c5
    Former-commit-id: d33311b2174f6f9c34a503e041efb3edbd29369f

--- js/umdah-script.js
+++ js/umdah-script.js
@@ -373,7 +373,7 @@ $(document).ready(() => {
         },
       },
 
-      info: "_TOTAL_ ޙަދީޘްގެ ތެރެއިން _START_ އަކުން _END_ އަކަށް",
+      info: "_TOTAL_ ޙަދީޘްގެ ތެރެއިން _START_ އިން_END_ އަކަށް",
       infoFiltered: "(ޖުމްލަ ބެލެވުނީ _MAX_)",
       infoEmpty: "— ނުފެނުނު —",
       lengthMenu: "_MENU_",
