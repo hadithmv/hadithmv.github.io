e2a79c049887453ae0cc0d6223f91f5842a1c372
hadithmv <yahyasdadmv@gmail.com>
2022-11-10 09:57:01 +0300

    u
    Former-commit-id: f8cf697e7a539cb0e58d28f630a18fccbc09e738

--- js/usooluThalaatha-script.js
+++ js/usooluThalaatha-script.js
@@ -66,9 +66,18 @@ $(document).ready(() => {
     data: usooluThalaatha_dataSet, // https://datatables.net/manual/ajax
 
     columns: [
-      {
+      /*{
         data: 0,
         title: "#",
+      },*/
+      {
+        /* add sofhaa string to page no */ data: 0,
+        title: "ސަފުހާ #",
+        render: function (data, type, row) {
+          // return data.replace(/َ/g, '').replace(/ِ/g, '')
+          // below code is shorter, no replace repeat, uses OR instead
+          return "[ސ. " + data + "]"; // prev ސަފުހާ
+        },
       },
       {
         data: 1,
@@ -458,13 +467,14 @@ $(document).ready(() => {
           // data = data.replace(/r\n]/g, '') // needed to make rnr work
           // data = data.replace(/\r\n|\n|\t/gm, '')
 
-          data = data.replace(/\r\n\r\n/g, " "); //  ONLY FOR 40N, because it has an inserted title
-          data = data.replace(/\n\n/g, " "); //  ONLY FOR 40N, because it has an inserted title, the two lines below arent needed for 40n i think
+          //data = data.replace(/\r\n\r\n/g, " "); //  What these two lines do is, they bring the hadith number right after the inserted title text.
+          //data = data.replace(/\n\n/g, " "); //  so these two are ok for hadith or point numbered books, but not for others like qurad radheef etc.
 
           data = data.replace(/\r\n/g, "\t"); //  prevents first header showing up unneeded (windows)
           data = data.replace(/\n/g, "\t"); //  prevents first header showing up unneeded (linux) this needs to go below windows rn
 
-          data = data.replace(/#\t/g, ""); // should be this way instead of /\tފޮތް/
+          //data = data.replace(/#\t/g, ""); // should be this way instead of /\tފޮތް/
+          data = data.replace(/ސަފުހާ #\t/g, "");
           data = data.replace(/އަރަބި ސުރުހީ\t/g, "");
           data = data.replace(/ދިވެހި ސުރުހީ\t/g, "");
           data = data.replace(/އަރަބި ނައްސު\t/g, "");
