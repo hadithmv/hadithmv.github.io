fd2d98fd5c5f75059b2bd5a88642a8e48e8dad93
hadithmv <yahyasdadmv@gmail.com>
2023-01-12 23:12:58 +0300

    u
    Former-commit-id: 39f58f453f0474a2d7908d206c8ddf3c83388aca [formerly 9b5fc56fb43c8e83c58b983e239abf0de4924042] [formerly ba7b9b39c0591298c9bd3021de312f785754b96a]
    Former-commit-id: bfe608cdda25b5f22b9d35b505ab29de041a2945
    Former-commit-id: 9b8ab3bbfcb169c7b04577e96f35422974014695

--- js/usooluSunnah-script.js
+++ js/usooluSunnah-script.js
@@ -66,9 +66,14 @@ $(document).ready(() => {
     data: usooluSunnah_dataSet, // https://datatables.net/manual/ajax
 
     columns: [
+      /* add # string to hadith no */
       {
         data: 0,
         title: "#",
+        render: function (data, type, row) {
+          // return data.replace(/َ/g, '').replace(/ِ/g, '') below code is shorter, no replace repeat, uses OR instead
+          return "# " + data;
+        },
       },
       {
         data: 1,
