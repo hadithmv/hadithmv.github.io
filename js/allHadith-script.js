4b4060e853f2d19ec720ac82752970352baa6977
hadithmv <yahyasdadmv@gmail.com>
2023-01-13 00:19:17 +0300

    u
    Former-commit-id: 85bdaa36fcda8fd3af6323b05efbb35de8f183dc
    Former-commit-id: 392c7bbdbcf7f2528c666a2d1fc47ac7ee83c694

--- js/allHadith-script.js
+++ js/allHadith-script.js
@@ -137,10 +137,8 @@ $(document).ready(() => {
         title: "އަރަބި ފިލިނުޖަހައި",
         render: function (data, type, row) {
           return data
-            .replace(
-              /[َ|ً|ُ|ٌ|ِ|ٍ|ْ|ّ|~|.|،|!|؟|-|ـ|’|”|:|؛|\{|\}|\(|\)|\[|\]|«|»|۝|⁽|⁾|\-|﴾|﴿|¹²³⁴⁵⁶⁷⁸⁹⁰]/g,
-              ""
-            )
+          .replace(/[َ|ً|ُ|ٌ|ِ|ٍ|ْ|ّ|~|⁽|⁾|¹²³⁴⁵⁶⁷⁸⁹⁰]/g, "")
+          // previously [َ|ً|ُ|ٌ|ِ|ٍ|ْ|ّ|~|.|،|!|؟|-|ـ|’|”|:|؛|\{|\}|\(|\)|\[|\]|«|»|۝|⁽|⁾|\-|﴾|﴿|¹²³⁴⁵⁶⁷⁸⁹⁰]
             .replace(/(\n\n)/g, "<br>");
           // the latter preserves newlines for fililess column
           //return data.replace(/[ًٌٍَُِّْ]/g, '')
--- js/umdah-script.js
+++ js/umdah-script.js
@@ -120,12 +120,12 @@ $(document).ready(() => {
         title: "އަރަބި ފިލިނުޖަހައި",
         render: function (data, type, row) {
           // return data.replace(/َ/g, '').replace(/ِ/g, '') below code is shorter, no replace repeat, uses OR instead
-          return data
-            .replace(
-              /[َ|ً|ُ|ٌ|ِ|ٍ|ْ|ّ|~|.|،|!|؟|-|ـ|’|”|:|؛|\{|\}|\(|\)|\[|\]|«|»|۝|⁽|⁾|\-|﴾|﴿|¹²³⁴⁵⁶⁷⁸⁹⁰]/g,
-              ""
-            )
-            .replace(/(\n\n)/g, "<br>");
+          return (
+            data
+              .replace(/[َ|ً|ُ|ٌ|ِ|ٍ|ْ|ّ|~|⁽|⁾|¹²³⁴⁵⁶⁷⁸⁹⁰]/g, "")
+              // previously [َ|ً|ُ|ٌ|ِ|ٍ|ْ|ّ|~|.|،|!|؟|-|ـ|’|”|:|؛|\{|\}|\(|\)|\[|\]|«|»|۝|⁽|⁾|\-|﴾|﴿|¹²³⁴⁵⁶⁷⁸⁹⁰]
+              .replace(/(\n\n)/g, "<br>")
+          );
           // the latter preserves newlines for fililess column
           //return data.replace(/[ًٌٍَُِّْ]/g, '')
         },
