49030836a641fb4388a0faf362d3656dace78151
hadithmv <yahyasdadmv@gmail.com>
2023-01-12 22:58:25 +0300

    u
    Former-commit-id: 858ee0c889086e9c5ecca190b5a80c060c776adc [formerly 18a6c04e84e496a099daab58f581104aa676a917]
    Former-commit-id: e2bae6c4bbd746b417daadd964f2173969ec5534

--- js/umdah-script.js
+++ js/umdah-script.js
@@ -66,9 +66,14 @@ $(document).ready(() => {
     data: umdah_dataSet, // https://datatables.net/manual/ajax
 
     columns: [
+      /* add # string to hadith no */
       {
         data: 0,
         title: "#",
+        render: function (data, type, row) {
+          // return data.replace(/َ/g, '').replace(/ِ/g, '') below code is shorter, no replace repeat, uses OR instead
+          return "# " + data;
+        },
       },
       {
         data: 1,
@@ -114,8 +119,7 @@ $(document).ready(() => {
         data: 5,
         title: "އަރަބި ފިލިނުޖަހައި",
         render: function (data, type, row) {
-          // return data.replace(/َ/g, '').replace(/ِ/g, '')
-          // below code is shorter, no replace repeat, uses OR instead
+          // return data.replace(/َ/g, '').replace(/ِ/g, '') below code is shorter, no replace repeat, uses OR instead
           return data
             .replace(
               /[َ|ً|ُ|ٌ|ِ|ٍ|ْ|ّ|~|.|،|!|؟|-|ـ|’|”|:|؛|\{|\}|\(|\)|\[|\]|«|»|۝|⁽|⁾|\-|﴾|﴿|¹²³⁴⁵⁶⁷⁸⁹⁰]/g,
