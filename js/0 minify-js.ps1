e57c9043628ebdfb2fc9a29f89e5519ce3deb2bf
hadithmv <yahyasdadmv@gmail.com>
2023-01-13 12:07:45 +0300

    u

--- js/0 minify-js.ps1
+++ js/0 minify-js.ps1
@@ -9,12 +9,12 @@ google-closure-compiler --charset=UTF-8 --js=eegaal-script.js --js_output_file=e
 google-closure-compiler --charset=UTF-8 --js=hisnulMuslim-script.js --js_output_file=hisnulMuslim-script.min.js
 google-closure-compiler --charset=UTF-8 --js=muwatta-script.js --js_output_file=muwatta-script.min.js
 google-closure-compiler --charset=UTF-8 --js=nawaqid-script.js --js_output_file=nawaqid-script.min.js
-google-closure-compiler --charset=UTF-8 --js=nawawi-script.js --js_output_file=nawawi-script.min.js
+google-closure-compiler --charset=UTF-8 --js=fortyNawawi-script.js --js_output_file=fortyNawawi-script.min.js
 google-closure-compiler --charset=UTF-8 --js=qawaid-script.js --js_output_file=qawaid-script.min.js
 google-closure-compiler --charset=UTF-8 --js=quran-script.js --js_output_file=quran-script.min.js
 google-closure-compiler --charset=UTF-8 --js=quranBakurube-script.js --js_output_file=quranBakurube-script.min.js
 google-closure-compiler --charset=UTF-8 --js=radheef-script.js --js_output_file=radheef-script.min.js
-google-closure-compiler --charset=UTF-8 --js=raziyain-script.js --js_output_file=raziyain-script.min.js
+google-closure-compiler --charset=UTF-8 --js=aqidatuRaziyain-script.js --js_output_file=aqidatuRaziyain-script.min.js
 google-closure-compiler --charset=UTF-8 --js=umdah-script.js --js_output_file=umdah-script.min.js
 google-closure-compiler --charset=UTF-8 --js=usooluThalaatha-script.js --js_output_file=usooluThalaatha-script.min.js
 google-closure-compiler --charset=UTF-8 --js=usooluSunnah-script.js --js_output_file=usooluSunnah-script.min.js
