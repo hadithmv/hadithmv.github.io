bd5f0436c828425df51aec584718448177ecdb6e
hadithmv <yahyasdadmv@gmail.com>
2023-01-16 01:09:48 +0300

    u
    Former-commit-id: 870ee85c28316faf94525cc8f36c2b788a6d7c23 [formerly d5f406bb119f07201dc94d3a67b2bd6e79ff5efc]
    Former-commit-id: 3a862afa8e5ef162f21853245082ed428a455f62

--- js/allHadith-script.js
+++ js/allHadith-script.js
@@ -72,7 +72,7 @@ $(document).ready(() => {
         title: "#",
         render: function (data, type, row) {
           // return data.replace(/َ/g, '').replace(/ِ/g, '') below code is shorter, no replace repeat, uses OR instead
-          return "# " + data;
+          return "#" + data;
         },
       },
       /* add brackets string to hukum */
