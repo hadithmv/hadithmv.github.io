6b091ffbee8b76a9e552783f92b705eb64e938ad
hadithmv <yahyasdadmv@gmail.com>
2023-01-17 12:55:11 +0300

    u
    Former-commit-id: f9fbec115e4b20068eed32bdd7d5673c8143e7a2 [formerly f97da082e11837eb44cde62692d19f73756f3a92] [formerly 154122398a80a4d68e890a98670da95c780515ae]
    Former-commit-id: de7460518228aa05efc2d689b1bb1306fd7b23c0
    Former-commit-id: 542c751b5f4c70ff706e3672113ffcc88024a016

--- js/0 minify-js-scripts.ps1
+++ js/0 minify-js-scripts.ps1
@@ -10,6 +10,7 @@ google-closure-compiler --charset=UTF-8 --js=hisnulMuslim-script.js --js_output_
 google-closure-compiler --charset=UTF-8 --js=muwatta-script.js --js_output_file=muwatta-script.min.js
 google-closure-compiler --charset=UTF-8 --js=nawaqidulislam-script.js --js_output_file=nawaqidulislam-script.min.js
 google-closure-compiler --charset=UTF-8 --js=fortyNawawi-script.js --js_output_file=fortyNawawi-script.min.js
+google-closure-compiler --charset=UTF-8 --js=fortyAajurry-script.js --js_output_file=fortyAajurry-script.min.js
 google-closure-compiler --charset=UTF-8 --js=qawaidulArbau-script.js --js_output_file=qawaidulArbau-script.min.js
 google-closure-compiler --charset=UTF-8 --js=quran-script.js --js_output_file=quran-script.min.js
 google-closure-compiler --charset=UTF-8 --js=quranBakurube-script.js --js_output_file=quranBakurube-script.min.js
