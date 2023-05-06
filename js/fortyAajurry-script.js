cd133e718be756dfcbe425dd41711a9393c8a371
hadithmv <yahyasdadmv@gmail.com>
2023-05-06 12:07:31 +0300

    u
    Former-commit-id: a728d32a44d53b72ecfa95b7b80b1633af77e607
    Former-commit-id: cce26c6f790cf32f57f49112614cc314904c416c

--- js/fortyAajurry-script.js
+++ js/fortyAajurry-script.js
@@ -656,7 +656,7 @@ $(document).ready(() => {
     $(".dataTable").on("page.dt", () => {
       $("html, body").animate(
         {
-          scrollTop: 190, //prev 0
+          scrollTop: 195, //prev 0
         },
         "fast"
       );
