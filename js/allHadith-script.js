cce26c6f790cf32f57f49112614cc314904c416c
hadithmv <yahyasdadmv@gmail.com>
2023-05-06 12:07:31 +0300

    u
    Former-commit-id: a728d32a44d53b72ecfa95b7b80b1633af77e607

--- js/allHadith-script.js
+++ js/allHadith-script.js
@@ -646,7 +646,7 @@ $(document).ready(() => {
     $(".dataTable").on("page.dt", () => {
       $("html, body").animate(
         {
-          scrollTop: 190, //prev 0 / 148
+          scrollTop: 195, //prev 0 / 148
         },
         "fast"
       );
