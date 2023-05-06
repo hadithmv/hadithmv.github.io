a728d32a44d53b72ecfa95b7b80b1633af77e607
hadithmv <yahyasdadmv@gmail.com>
2023-05-06 12:07:31 +0300

    u

--- js/eegaal-script.js
+++ js/eegaal-script.js
@@ -592,7 +592,7 @@ $(document).ready(() => {
     $(".dataTable").on("page.dt", () => {
       $("html, body").animate(
         {
-          scrollTop: 190, //prev 0
+          scrollTop: 195, //prev 0
         },
         "fast"
       );
