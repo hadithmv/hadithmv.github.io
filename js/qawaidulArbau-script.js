97ba74851b1db407f57cd4fd957216e4c9d62e11
hadithmv <yahyasdadmv@gmail.com>
2023-05-10 03:39:09 +0300

    u

--- js/qawaidulArbau-script.js
+++ js/qawaidulArbau-script.js
@@ -116,7 +116,7 @@ $(document).ready(() => {
       },
       {
         data: 6,
-        title: "ތަޚްރީޖު އަރަބިން",
+        title: "ތަޚްރީޖު",
       },
       {
         data: 7,
@@ -476,7 +476,7 @@ $(document).ready(() => {
           data = data.replace(/އަރަބި ފިލިނުޖަހައި\t/g, "");
           data = data.replace(/ދިވެހި ތަރުޖަމާ\t/g, "");
           data = data.replace(/English\t/g, "");
-          data = data.replace(/ތަޚްރީޖު އަރަބިން\t/g, "");
+          data = data.replace(/ތަޚްރީޖު\t/g, "");
           data = data.replace(/شرح الفوزان\t/g, "");
           data = data.replace(/شرح آل الشيخ\t/g, "");
 
