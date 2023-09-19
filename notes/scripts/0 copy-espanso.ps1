a657a64ad1c5c75673ebf230ff1cba1044752a87
hadithmv <yahyasdadmv@gmail.com>
2023-09-19 16:32:09 +0500

    u
    Former-commit-id: aff378faad4e520ffd7c641a892fd12c51464548

--- notes/scripts/0 copy-espanso.ps1
+++ notes/scripts/0 copy-espanso.ps1
@@ -2,4 +2,6 @@
 
 Set-Location -Path $PSScriptRoot #C:/Users/ashraaf/Downloads/VScode/hadithmv.github.io/uc
 
-Copy-Item base.yml -Destination C:\Users\hadit\AppData\Roaming\espanso\match
+# https://cannotdisplay.com/getting-the-path-of-appdata-using-powershell/
+# $env:APPDATA = C:\Users\user\AppData\Roaming
+Copy-Item base.yml -Destination $env:APPDATA\espanso\match
