1181064e7b4cc5c94e43584eeb281c327587107c
hadithmv <yahyasdadmv@gmail.com>
2023-12-23 21:07:49 +0500

    u
    Former-commit-id: cb293663840476c08828839c33f391e3f23bee65
    Former-commit-id: 7862ef182e1b79e23e8dedae05bc7d48f450602f

--- js/0 minify-js-scripts.ps1
+++ js/0 minify-js-scripts.ps1
@@ -4,14 +4,16 @@ Set-Location -Path $PSScriptRoot #C:/Users/ashraaf/Downloads/VScode/hadithmv.git
 
 google-closure-compiler --charset=UTF-8 --js=allAthar-script.js --js_output_file=allAthar-script.min.js
 google-closure-compiler --charset=UTF-8 --js=aqidahNawawi-script.js --js_output_file=aqidahNawawi-script.min.js
+google-closure-compiler --charset=UTF-8 --js=aqidatuRaziyain-script.js --js_output_file=aqidatuRaziyain-script.min.js
 google-closure-compiler --charset=UTF-8 --js=barbahari-script.js --js_output_file=barbahari-script.min.js
 google-closure-compiler --charset=UTF-8 --js=bulugh-script.js --js_output_file=bulugh-script.min.js
 google-closure-compiler --charset=UTF-8 --js=eegaal-script.js --js_output_file=eegaal-script.min.js
+google-closure-compiler --charset=UTF-8 --js=fortyAajurry-script.js --js_output_file=fortyAajurry-script.min.js
+google-closure-compiler --charset=UTF-8 --js=fortyNawawi-script.js --js_output_file=fortyNawawi-script.min.js
 google-closure-compiler --charset=UTF-8 --js=hisnulMuslim-script.js --js_output_file=hisnulMuslim-script.min.js
+google-closure-compiler --charset=UTF-8 --js=kuruLiyunthah-script.js --js_output_file=kuruLiyunthah-script.min.js
 google-closure-compiler --charset=UTF-8 --js=muwatta-script.js --js_output_file=muwatta-script.min.js
 google-closure-compiler --charset=UTF-8 --js=nawaqidulislam-script.js --js_output_file=nawaqidulislam-script.min.js
-google-closure-compiler --charset=UTF-8 --js=fortyNawawi-script.js --js_output_file=fortyNawawi-script.min.js
-google-closure-compiler --charset=UTF-8 --js=fortyAajurry-script.js --js_output_file=fortyAajurry-script.min.js
 google-closure-compiler --charset=UTF-8 --js=qawaidulArbau-script.js --js_output_file=qawaidulArbau-script.min.js
 google-closure-compiler --charset=UTF-8 --js=quranBakurube-script.js --js_output_file=quranBakurube-script.min.js
 google-closure-compiler --charset=UTF-8 --js=quranHmv-script.js --js_output_file=quranHmv-script.min.js
@@ -19,7 +21,6 @@ google-closure-compiler --charset=UTF-8 --js=quranJaufar-script.js --js_output_f
 google-closure-compiler --charset=UTF-8 --js=quranRasmee-script.js --js_output_file=quranRasmee-script.min.js
 google-closure-compiler --charset=UTF-8 --js=quranSoabuni-script.js --js_output_file=quranSoabuni-script.min.js
 google-closure-compiler --charset=UTF-8 --js=radheef-script.js --js_output_file=radheef-script.min.js
-google-closure-compiler --charset=UTF-8 --js=aqidatuRaziyain-script.js --js_output_file=aqidatuRaziyain-script.min.js
 google-closure-compiler --charset=UTF-8 --js=riyadusaliheen-script.js --js_output_file=riyadusaliheen-script.min.js
 google-closure-compiler --charset=UTF-8 --js=umdah-script.js --js_output_file=umdah-script.min.js
 google-closure-compiler --charset=UTF-8 --js=ushrulAkheer-script.js --js_output_file=ushrulAkheer-script.min.js
