51fa351872a9f46d577c153421bd53bad495795e
hadithmv <yahyasdadmv@gmail.com>
2023-12-26 18:03:30 +0500

    u
    Former-commit-id: dc4f5eca7ad715aafcbb2fe6063bae88438d1d8c
    Former-commit-id: c8339a847989130c6faf9c1dd28fe29f8db0b562

--- js/quranHmv-script.js
+++ js/quranHmv-script.js
@@ -773,7 +773,7 @@ $(document).ready(() => {
         extend: "copy",
         key: { key: "c", shiftKey: true },
         text: "ކޮޕީ",
-        messageTop: "ޙަދީޘްއެމްވީ – ގުރްއާނުގެ ތަރުޖަމާ", // CHANGE123 clipboard message
+        messageTop: "ޙަދީޘްއެމްވީ ގުރްއާން ތަރުޖަމާ", // CHANGE123 clipboard message
         title: "" /* title: "hadithmv.com", */,
 
         //= ====================
