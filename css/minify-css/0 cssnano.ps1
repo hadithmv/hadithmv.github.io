1ad43c95265b55e3363d0178dcf68ddb8e493dc1
Mohamed Ashraaf Ibrahim <yahyasdadmv@gmail.com>
2024-08-13 20:05:59 +0500

    u

--- css/minify-css/0 cssnano.ps1
+++ css/minify-css/0 cssnano.ps1
@@ -4,25 +4,25 @@ Set-Location -Path $PSScriptRoot
 # Define the target directory
 $targetDir = "C:\Users\ashra\AppData\Roaming\npm\node_modules"
 
-# Get all .css files in the current directory
+# Get all relevant files in the current directory
 $cssFiles = Get-ChildItem -Filter *.css
 
-# Loop through each .css file
+# Loop through each relevant file
 foreach ($file in $cssFiles) {
     # Define the target path for copying
     $targetPath = Join-Path -Path $targetDir -ChildPath $file.Name
     
-    # Copy the .css file to the target directory
+    # Copy the relevant file to the target directory
     Copy-Item -Path $file.FullName -Destination $targetPath
     
     # Generate the output file name
     $outputFile = $file.BaseName + "-nano.min.css"
     $outputFilePath = Join-Path -Path $PSScriptRoot -ChildPath $outputFile
     
-    # Run postcss on the copied file
+    # Minify the copied file
     & postcss $targetPath > $outputFilePath
     
-    # Delete the original .css file in the target directory
+    # Delete the original relevant file in the target directory
     Remove-Item -Path $targetPath
     
     # Output progress
