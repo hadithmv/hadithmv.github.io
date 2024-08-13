60be49584ad66344af72da81fef44dcb476e46cc
Mohamed Ashraaf Ibrahim <yahyasdadmv@gmail.com>
2024-08-13 20:05:59 +0500

    u
    Former-commit-id: 1ad43c95265b55e3363d0178dcf68ddb8e493dc1

--- js/resc/minify-js/0 closure-compiler.ps1
+++ js/resc/minify-js/0 closure-compiler.ps1
@@ -1,14 +1,14 @@
 # Change the current directory to the script's location
 Set-Location -Path $PSScriptRoot
 
-# Get all .js files in the current directory
+# Get all relevant files in the current directory
 $jsFiles = Get-ChildItem -Filter *.js
-# Loop through each .js file
+# Loop through each relevant file
 foreach ($file in $jsFiles) {
     # Generate the output file name
     $outputFile = $file.BaseName + "-c.min.js"
     
-    # Run Google Closure Compiler on the file
+    # Minify the file
     & google-closure-compiler --charset=UTF-8 --js $file.Name --js_output_file $outputFile
     
     # Output progress
