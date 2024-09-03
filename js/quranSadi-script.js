9fc0be15ef9aa3589ba4393f5a7283c1ded2ac89
Mohamed Ashraaf Ibrahim <yahyasdadmv@gmail.com>
2024-09-04 01:07:34 +0500

    u
    Former-commit-id: 4c2f114b39b1b24179c6d71e8667c8c255a18c84
    Former-commit-id: e8b2e33aee510f3f08a0c57a5ef34cfc1f0b5680

--- js/quranSadi-script.js
+++ js/quranSadi-script.js
@@ -322,7 +322,7 @@ $(document).ready(() => {
       },
       {
         data: 6,
-        title: "ތަފުސީރު", // ubufili on faafu, otherwise interferes with clipboard tite copy
+        title: "ތަފްސީރު", // ubufili on faafu, otherwise interferes with clipboard tite copy
       },
       {
         data: 6,
