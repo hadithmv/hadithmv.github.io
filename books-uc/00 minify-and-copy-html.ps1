4222263beaf67e4170cec0744d1edde50a8c9385
Mohamed Ashraaf Ibrahim <yahyasdadmv@gmail.com>
2024-09-06 02:51:45 +0500

    u
    Former-commit-id: 1eba0f467b9c24142e852b6f9fba2259ed131fff
    Former-commit-id: d4557db046efcc673f0e6e0f281de2668910580e

--- books-uc/00 minify-and-copy-html.ps1
+++ books-uc/00 minify-and-copy-html.ps1
@@ -3,72 +3,29 @@ Set-Location -Path $PSScriptRoot
 
 # Define a function to minify HTML files
 function MinifyHTML($inputFile, $outputFile) {
- # Use html-minifier with various options to minify the HTML file
- html-minifier --collapse-boolean-attributes --collapse-whitespace --decode-entities --minify-css true --minify-js true --process-scripts [text/html] --remove-attribute-quotes --remove-comments --remove-empty-attributes --remove-optional-tags --remove-redundant-attributes --remove-script-type-attributes --remove-style-link-type-attributes --remove-tag-whitespace --sort-attributes --sort-class-name --trim-custom-fragments --use-short-doctype $inputFile -o $outputFile
+    # Use html-minifier with various options to minify the HTML file
+    html-minifier --collapse-boolean-attributes --collapse-whitespace --decode-entities --minify-css true --minify-js true --process-scripts [text/html] --remove-attribute-quotes --remove-comments --remove-empty-attributes --remove-optional-tags --remove-redundant-attributes --remove-script-type-attributes --remove-style-link-type-attributes --remove-tag-whitespace --sort-attributes --sort-class-name --trim-custom-fragments --use-short-doctype $inputFile -o $outputFile
 }
 
-# Define an array of file names to be processed (without the .html extension)
-$files = @(
-"akhbaruShuyukh",
-"akhlaqHamalathilQuran",
-"allAqida",
-"allAthar",
-"akhbaruShuyukh",
-"akhlaqHamalathilQuran",
-"allAqida",
-"allAthar",
-"aqidatuRaziyain",
-"aqidahNawawi",
-"sharhuSunnahBarbahari",
-"sharhuSunnahBarbahari-DFK",
-"bulughulMaram",
-"bulughulMaramFull",
-"arbaoonNawawi",
-"arbaoonAajurry",
-"hisnulMuslim",
-"intisarLiAshabilHadith",
-"kitabulEmanAbiUbaid",
-"kunnaasha",
-"index",
-"muwattaMalik",
-"radheefNanfoiy",
-"nawaqidulislam",
-"qawaidulArbau",
-"quranBakurube",
-"quranHadithmv",
-"quranJaufar",
-"quranSoabuni",
-"radheefAll",
-"radheefEegaal",
-"radheefManiku",
-"radheefRasmee",
-"riyaduSaliheen",
-"umdathulAhkam",
-"quranUshru",
-"usooluSiththa",
-"usooluSunnahAhmed",
-"usooluThalaatha"
-)
+# Get all HTML files in the current directory
+$files = Get-ChildItem -Filter "*.html"
 
-# "quranMuhammad",
-# "quranBetaqat",
-# "quranMukhtasar",
-# "quranMuyassar",
-# "quranMuyassarGhareeb",
-# "quranQiraaath",
-# "quranRasmee",
-# "quranSadi",
-
-# Loop through each file in the array
+# Loop through each HTML file
 foreach ($file in $files) {
- # Construct the input file path by adding .html to the file name
- $inputFile = "$file.html"
- # Construct the output file path, placing the file in the ../books/ directory
- $outputFile = "../books/$file.html"
- # Call the MinifyHTML function to process the file
- MinifyHTML $inputFile $outputFile
- # Print a message indicating that the file has been processed
- Write-Output "Processed: $file.html"
+    # Get the file name without extension
+    $baseName = [System.IO.Path]::GetFileNameWithoutExtension($file.Name)
+    
+    # Construct the input file path
+    $inputFile = $file.FullName
+    
+    # Construct the output file path, placing the file in the ../page/ directory
+    $outputFile = "../page/$($file.Name)"
+    
+    # Call the MinifyHTML function to process the file
+    MinifyHTML $inputFile $outputFile
+    
+    # Print a message indicating that the file has been processed
+    Write-Output "Processed: $($file.Name)"
 }
 
 # SEPARATE
@@ -76,30 +33,7 @@ foreach ($file in $files) {
 # copies index page over to layout index dir
 Copy-Item "../books/index.html" -Destination "../_layouts/index.html"
 
+# copies 404 to root
+#Copy-Item "../page/404.html" -Destination "../404.html"
+
 Write-Output "✅ -- ✅ -- DONE -- ✅ -- ✅"
-
-#
-
-#
-
-# OLD:
-
-<#
-function MinifyHTML($inputFile, $outputFile) {
- html-minifier --collapse-boolean-attributes --collapse-whitespace --decode-entities --minify-css true --minify-js true --process-scripts [text/html] --remove-attribute-quotes --remove-comments --remove-empty-attributes --remove-optional-tags --remove-redundant-attributes --remove-script-type-attributes --remove-style-link-type-attributes --remove-tag-whitespace --sort-attributes --sort-class-name --trim-custom-fragments --use-short-doctype $inputFile -o $outputFile
-}
-
-MinifyHTML "akhbaruShuyukh.html" "../books/akhbaruShuyukh.html"
-MinifyHTML "akhlaqHamalathilQuran.html" "../books/akhlaqHamalathilQuran.html"
-MinifyHTML "allAqida.html" "../books/allAqida.html"...
-
-# OLDER
-
-# copies index page over to layout index dir
-#>
-# Set-Location -Path $PSScriptRoot #C:/Users/ashraaf/Downloads/VScode/hadithmv.github.io/uc
-# html-minifier --collapse-boolean-attributes --collapse-whitespace --decode-entities --minify-css true --minify-js true --process-scripts [text/html] --remove-attribute-quotes --remove-comments --remove-empty-attributes --remove-optional-tags --remove-redundant-attributes --remove-script-type-attributes --remove-style-link-type-attributes --remove-tag-whitespace --sort-attributes --sort-class-name --trim-custom-fragments --use-short-doctype akhbaruShuyukh.html -o ../books/akhbaruShuyukh.html
-
-
-# minifies (commented out below line because it copies over the whole dir, including test files)
-# html-minifier --input-dir $PSScriptRoot --output-dir ../books --file-ext html --collapse-boolean-attributes --collapse-whitespace --decode-entities --minify-css true --minify-js true --process-scripts [text/html] --remove-attribute-quotes --remove-comments --remove-empty-attributes --remove-optional-tags --remove-redundant-attributes --remove-script-type-attributes --remove-style-link-type-attributes --remove-tag-whitespace --sort-attributes --sort-class-name --trim-custom-fragments --use-short-doctype
