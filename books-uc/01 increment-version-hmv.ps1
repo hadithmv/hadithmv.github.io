16fc3e5a920c5b2f55ae04a4fbf643dbe7c3bd9e
Mohamed Ashraaf Ibrahim <yahyasdadmv@gmail.com>
2025-03-16 07:13:50 +0500

    u
    Former-commit-id: 68125e7ac4b06bff98f04d5b45ff467b9c6cb060
    Former-commit-id: 945cd70e88f9e763eed547eddea8fdae49a09bd5

--- books-uc/01 increment-version-hmv.ps1
+++ books-uc/01 increment-version-hmv.ps1
@@ -21,6 +21,7 @@ catch {
 # Files to update versions in
 $filesToUpdate = @(
     @{Path = "..\js\navbar.js"; Pattern = 'var hmvVersionNo = "(\d+\.\d+\.\d+)";'; Replacement = 'var hmvVersionNo = "{0}";'},
+    @{Path = "..\js\navbar.min.js"; Pattern = 'var hmvVersionNo="(\d+\.\d+\.\d+)"'; Replacement = 'var hmvVersionNo="{0}"'},
     @{Path = "..\windowsApp-tauri\Hadithmv\src-tauri\tauri.conf.json"; Pattern = '"version": "(\d+\.\d+\.\d+)"'; Replacement = '"version": "{0}"'},
     @{Path = "..\androidApp-kt\app\build.gradle"; Pattern = 'versionName\s+"([^"]+)"'; Replacement = 'versionName "{0}"'}
 )
